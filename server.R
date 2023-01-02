7b436958eba08c6ec741160bd8c7204d4f088e43
Mr-T001 <56784576+Mr-T001@users.noreply.github.com>
2023-01-02 23:07:46 +0000

    Add files via upload

---
+++ server.R
@@ -0,0 +1,31 @@
+library(data.table)
+library(randomForest)
+
+shinyServer(function(input, output, session) {
+  
+  # Input Data
+  datasetInput <- reactive({  
+    
+    bmi <- input$weight/( (input$height/100) * (input$height/100) )
+    bmi <- data.frame(bmi)
+    names(bmi) <- "BMI"
+    print(bmi)
+    
+  })
+  
+  # Status/Output Text Box
+  output$contents <- renderPrint({
+    if (input$submitbutton>0) { 
+      isolate("Calculation complete.") 
+    } else {
+      return("Server is ready for calculation.")
+    }
+  })
+  
+  # Prediction results table
+  output$tabledata <- renderTable({
+    if (input$submitbutton>0) { 
+      isolate(datasetInput()) 
+    } 
+  })
+})
